d0f0a4c2e036aa3f530194f3450ef134c4832d55
tuxiantian <tuxiantian@163.com>
2020-04-24 21:10:12 +0800

    使用toast-ui

--- src/components/MarkdownEditor/index.vue
+++ src/components/MarkdownEditor/index.vue
@@ -5,10 +5,13 @@
 <script>
 // deps for editor
 import 'codemirror/lib/codemirror.css' // codemirror
-import 'tui-editor/dist/tui-editor.css' // editor ui
-import 'tui-editor/dist/tui-editor-contents.css' // editor content
+import '@toast-ui/editor/dist/toastui-editor.css' // editor ui
 
-import Editor from 'tui-editor'
+import Editor from '@toast-ui/editor';
+import chart from '@toast-ui/editor-plugin-chart';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+import tableMergedCell from '@toast-ui/editor-plugin-table-merged-cell';
+
 import defaultOptions from './default-options'
 
 export default {
@@ -57,6 +60,7 @@ export default {
       options.initialEditType = this.mode
       options.height = this.height
       options.language = this.language
+      options.plugins = [chart,codeSyntaxHighlight,tableMergedCell]
       return options
     }
   },
@@ -89,23 +93,11 @@ export default {
         el: document.getElementById(this.id),
         ...this.editorOptions
       })
-      if (this.value) {
-        this.editor.setValue(this.value)
-      }
-      this.editor.on('change', () => {
-        this.$emit('input', this.editor.getValue())
-      })
     },
     destroyEditor() {
       if (!this.editor) return
       this.editor.off('change')
       this.editor.remove()
-    },
-    setValue(value) {
-      this.editor.setValue(value)
-    },
-    getValue() {
-      return this.editor.getValue()
     },
     setHtml(value) {
       this.editor.setHtml(value)

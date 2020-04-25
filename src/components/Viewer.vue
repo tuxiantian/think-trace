36bd6f01d56b068011d71a2d4f7269a75bd0c40f
tuxiantian <tuxiantian@163.com>
2020-04-25 21:54:02 +0800

    引入Markdown viewer

---
+++ src/components/Viewer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="viewer"></div>
+</template>
+
+<script>
+import "@toast-ui/editor/dist/toastui-editor-viewer.css";
+import Viewer from "@toast-ui/editor/dist/toastui-editor-viewer";
+import { getArticle } from "../api/api.js";
+
+export default {
+  name: "Viewer",
+  data() {
+    return {
+      id: null,
+      title: "",
+      content: ""
+    };
+  },
+  created() {
+    this.id = this.$route.params.id;
+    getArticle(this.id).then(data => {
+      this.title = data.title;
+      this.content = data.content;
+      const viewer = new Viewer({
+        el: document.querySelector("#viewer"),
+        height: "500px",
+        viewer: true,
+        initialValue: data.content
+      });
+    });
+  }
+};
+</script>
